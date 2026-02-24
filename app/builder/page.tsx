1027ec97d6dc2dc98111dd1c5a9bc2eb57072730
Admin <161949841+admin-devsec@users.noreply.github.com>
2026-02-24 14:16:24 +0000

    Fix builder page dynamic import in server component

--- app/builder/page.tsx
+++ app/builder/page.tsx
@@ -1,8 +1,4 @@
-import dynamic from 'next/dynamic';
-
-const BuilderEditor = dynamic(() => import('@/components/builder/BuilderEditor'), {
-  ssr: false
-});
+import BuilderEditor from '@/components/builder/BuilderEditor';
 
 export default function BuilderPage() {
   return <BuilderEditor />;
